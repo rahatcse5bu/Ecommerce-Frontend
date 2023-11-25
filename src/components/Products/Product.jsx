ac9c3909b919ace8bf64b06fef548ff5d3b466aa
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-25 07:46:06 +0600

    some changes

--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -9,10 +9,13 @@ const Product = (product) => {
             <div className='absolute top-0 right-0 bg-blue-900 text-white px-2 py-0 rounded-[200px]' >{singleProduct.category}</div>
             <img className='rounded-md' src={singleProduct.img}/>
             <h4 className='font-bold text-lg text-center'>{singleProduct.title}</h4>
-       <div className='flex flex-row align-middle justify-center items-center'> <RatingStar rating={singleProduct.rating}/> <span className='ml-2'>{singleProduct.reviews}</span></div>
-<div className='flex flex-row align-middle justify-center items-center'>
-<span className='line-through mr-2'>{singleProduct.prevPrice} </span> <span> {singleProduct.newPrice}</span>
+       <div className='flex flex-row align-middle justify-center items-center py-2'>
+        <RatingStar rating={singleProduct.rating}/> <div className='ml-2'>{singleProduct.reviews}</div>
         </div>
+<div className='flex flex-row align-middle justify-center items-center py-2'>
+<span className='line-through mr-4'>{singleProduct.prevPrice} </span> <span> ${singleProduct.newPrice}</span>
+        </div>
+        {/* <div className='brand text-center bg-slate-400 text-white mx-auto rounded-sm p-1 flex items-center justify-center'>Brand: {singleProduct.company}</div> */}
         </div>
     );
 };
